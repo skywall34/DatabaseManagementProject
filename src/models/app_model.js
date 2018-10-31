0029908de9f6b7ac1c2cf3b7f0b7d20ae907548c
skywall34 <doshinkorean@gmail.com>
2018-10-31 00:10:47 -0500

    basic mysql commands for all table added

--- src/models/app_model.js
+++ src/models/app_model.js
@@ -1,2 +1,177 @@
 // has the basic database functions
 // INSERT, DELETE, and SELECT
+let express = require('express');
+let sql = require('../server');
+
+//Create a constructor for each table
+
+let User = (user) => {
+  this.username = user.username;
+  this.password = user.password;
+  this.balance = user.balance;
+  this.is_admin = user.is_admin;
+};
+
+let Giftcard = {};
+
+let Transaction = (transaction) => {
+    this.from_user = transaction.from_user;
+    this.to_user = transaction.to_user;
+    this.trans_date = new Date();
+    this.amount = transaction.amount;
+};
+
+// User Functions
+User.get_all_users = (result) => {
+        sql.query("Select * from users", function (err, res) {
+
+                if(err) {
+                    console.log("error: ", err);
+                    result(null, err);
+                }
+                else{
+                  console.log('tasks : ', res);
+                  result(null, res);
+                }
+            });
+};
+
+
+User.update_user_by_id = (user_id, target_column, target_value, result) => {
+  sql.query(`UPDATE tasks SET ${target_column} = ${target_value} WHERE user_id = ${user_id}`,  function (err, res) {
+          if(err) {
+              console.log("error: ", err);
+              result(null, err);
+             }
+           else{
+             result(null, res);
+                }
+            });
+};
+
+/*
+User.remove_user = function(id, result){
+     sql.query(`DELETE FROM users WHERE user_id = ${user_id}`, function (err, res) {
+
+                if(err) {
+                    console.log("error: ", err);
+                    result(null, err);
+                }
+                else{
+
+                 result(null, res);
+                }
+            });
+};
+*/
+
+// Giftcard functions
+
+Giftcard.create_card = (new_card, result) => {
+        sql.query(`INSERT INTO tasks set ${new_card}`, function (err, res) {
+
+                if(err) {
+                    console.log("error: ", err);
+                    result(err, null);
+                }
+                else{
+                    console.log(res.insert_id);
+                    result(null, res.insert_id);
+                }
+            });
+};
+
+Giftcard.get_all_cards = (result) => {
+        sql.query("Select * from giftcard", function (err, res) {
+
+                if(err) {
+                    console.log("error: ", err);
+                    result(null, err);
+                }
+                else{
+                  console.log('tasks : ', res);
+                  result(null, res);
+                }
+            });
+};
+
+
+
+Giftcard.remove_card = (id, result) => {
+     sql.query(`DELETE FROM giftcard WHERE user_id = ${user_id}`, function (err, res) {
+
+                if(err) {
+                    console.log("error: ", err);
+                    result(null, err);
+                }
+                else{
+                  result(null, res);
+                }
+            });
+};
+
+//Transaction Model Functions
+Transaction.create_transaction = (new_transaction, result) => {
+        sql.query(`INSERT INTO transactions SET ${new_transaction}`, function (err, res) {
+
+                if(err) {
+                    console.log("error: ", err);
+                    result(err, null);
+                }
+                else{
+                    console.log(res.insert_id);
+                    result(null, res.insert_id);
+                }
+            });
+};
+Transaction.get_transaction_by_id = (transaction_id, result) => {
+        sql.query(`SELECT * FROM transactions WHERE transaction_id = ${transaction_id} `,function (err, res) {
+                if(err) {
+                    console.log("error: ", err);
+                    result(err, null);
+                }
+                else{
+                    result(null, res);
+
+                }
+            });
+};
+Transaction.get_all_transactions = (result) => {
+        sql.query("SELECT * FROM transactions", function (err, res) {
+
+                if(err) {
+                    console.log("error: ", err);
+                    result(null, err);
+                }
+                else{
+                  console.log('tasks : ', res);
+                  result(null, res);
+                }
+            });
+};
+Transaction.update_transaction_by_id = (transaction_id, target_column, target_value, result) => {
+  sql.query(`UPDATE transactions SET ${target_column} = ${target_value} WHERE transaction_id = ${transaction_id}`,  function (err, res) {
+          if(err) {
+              console.log("error: ", err);
+              result(null, err);
+             }
+           else{
+             result(null, res);
+                }
+            });
+};
+Transaction.remove_transaction = (transaction_id, result) => {
+     sql.query(`DELETE FROM transactions WHERE transaction_id = ${transaction_id}`, function (err, res) {
+
+                if(err) {
+                    console.log("error: ", err);
+                    result(null, err);
+                }
+                else{
+                  result(null, res);
+                }
+            });
+};
+
+
+module.exports= {User, Giftcard, Transaction};
--- src/routes/app_route.js
+++ src/routes/app_route.js
@@ -15,11 +15,56 @@ router.get('/tasks', (req, res) => {
   });
 });
 
+
+// QueryString => query property on the request object
+// localhost:3000/person?name=thomas&age=20
+router.get('/person', (req, res) => {
+        if(req.query.name){
+                res.send(`You have requested a person $(req.query.name)`);
+        }
+        else{
+                res.send('You have requested a person');
+        }
+})
+
+// Params property on the request object
+// localhost:3000/person/thomas
+router.get('/person/:name', (req, res) => {
+        res.send(`You have requested a person ${req.params.name}`);
+})
+
+
+- the first one calls a query string as input
+- the second one takes the query as part of the route and not as a query string
+
 */
 
 let AccountModel = require('../models/app_model.js');
 let express = require('express');
 let router = express.Router();
+
+//get the table objects
+let User = AccountModel.User;
+let Giftcard = AccountModel.Giftcard;
+let Transaction = AccountModel.Transaction;
+
+
+
+router.get('/users', (req, res) => {
+  User.get_all_users((err, user) => {
+    console.log('router get users');
+    if (err){
+      res.send(err);
+      console.log('res', user);
+    } else {
+      res.send(user);
+    }
+  });
+});
+
+
+
+
 
 
 module.exports = router;
